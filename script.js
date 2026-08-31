61c8c3580d4f7f21b660180c839908cbd0e31d64
gabriel angelo <gabriel.angelo7002@gmail.com>
2026-08-31 16:53:39 -0300

    style3

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const card = document.querySelectorAll(".card");
 card.forEach(function(card) {
 
     const text = card.querySelector(".card_name")
+    const texto = card.querySelector("h2")
 
     card.addEventListener("mouseenter", function() {
         card.classList.add("borda1");
         text.classList.add("vermelho");
+        texto.classList.add("preto")
     });
 
     card.addEventListener("mouseleave", function() {
         card.classList.remove("borda1");
         text.classList.remove("vermelho");
+        texto.classList.remove("preto")
     });
 
 });
